356b902f91c119877e43a89763efafb77eab313c
Jorge Rangel Jimenez <rangel2016sistemas@gmail.com>
2018-10-02 06:15:40 -0500

    Variables JS

---
+++ Clase 1 Variables/index.js
@@ -0,0 +1,21 @@
+// Definir una variable
+var nombre;
+var apellido;
+
+// Asignando valor a una variable
+nombre = 'Jorge';
+apellido = 'Rangel'; 
+
+// Imprimir en consola
+console.log('Hola ' + nombre + ' ' + apellido)
+
+// Definir variables con valor asignado en una sola linea
+var direccion = 'Calle 11 1 11', ciudad = 'Bogota';
+
+console.log('direccion: ' + direccion + ' y ciudad: '+ ciudad);
+
+// Debilidad tipado de variables en Javascript
+var edad = 23;
+console.log('Tengo edad ' + edad + ' Años');
+
+var peso = 65;
