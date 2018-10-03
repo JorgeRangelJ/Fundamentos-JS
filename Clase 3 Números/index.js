9b767725c6b771fbfc1a71f0fa6ae4d814f8683b
Jorge Rangel Jimenez <rangel2016sistemas@gmail.com>
2018-10-03 06:17:30 -0500

    Números

---
+++ Clase 3 Números/index.js
@@ -0,0 +1,29 @@
+// Definir una variable
+var edad = 23;
+
+// Incremento a una variable
+edad += 1;
+
+var peso = 65;
+// Decremento a una variable
+peso -= 2;
+
+var sandwich = 1;
+peso += sandwich;
+
+var jugarFutbol = 3;
+
+peso -= jugarFutbol;
+
+// Decimales
+
+var preciovino = 200.3;
+
+var total = Math.round(preciovino * 100 * 3) / 100;
+var totalStr = total.toFixed(2);
+var totalNumero = parseFloat(totalStr);
+
+var pizza = 8;
+var persona = 2;
+
+var cantidadDePorcionesPorPersona = pizza / persona;
