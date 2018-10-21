6546cd1154a65abec024f1306c749b11e5ef721d
Jorge Rangel Jimenez <rangel2016sistemas@gmail.com>
2018-10-20 19:17:59 -0500

    Comparaciones en JavaScript

---
+++ clase 9 Comparaciones en JavaScript/index.js
@@ -0,0 +1,18 @@
+// Comparaciones en JavaScript
+
+// comparar variables
+var x = 4, y = '4';
+
+console.log(x === y);
+
+// comparar objetos
+
+var persona = {
+	nombre: 'jorge'
+}
+
+var otraPersona = {
+	nombre: 'jorge'
+}
+
+console.log(persona === otraPersona);
